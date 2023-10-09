5b8cf3c17f760d21f88295a4e3ab648a88635759
LLZed <145909255+LLZed@users.noreply.github.com>
2023-10-09 10:01:47 +0800

    Create test4.c

---
+++ Chapter3/test4.c
@@ -0,0 +1,12 @@
+//分析程序
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    char c1,c2;
+    c1=97;
+    c2=98;
+    printf("c1=%c,c2=%c\n",c1,c2);
+    printf("c1=%d,c2=%d\n",c1,c2);
+    system("pause");
+    return 0;
+}
